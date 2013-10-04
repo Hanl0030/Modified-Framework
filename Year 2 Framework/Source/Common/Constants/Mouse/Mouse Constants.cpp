763a5a9e6674da9d149f34598d7b3ba989041e56
Hanl0030 <Hanl0030@users.noreply.github.com>
2013-10-03 23:12:37 -0400

    Delete Mouse Constants.cpp

--- Year 2 Framework/Source/Common/Constants/Mouse/Mouse Constants.cpp
+++
@@ -1,9 +0,0 @@
-//Mouse State Constants
-
-const unsigned int STATE_MOUSE_LEFT_DOWN     = 1;
-const unsigned int STATE_MOUSE_LEFT_UP       = 2;
-const unsigned int STATE_MOUSE_RIGHT_DOWN    = 3;
-const unsigned int STATE_MOUSE_RIGHT_UP      = 4;
-const unsigned int STATE_MOUSE_MOVE          = 5;
-const unsigned int STATE_MOUSE_NULL          = 6;
-const unsigned int STATE_MOUSE_HOVER         = 7;
